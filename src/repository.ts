c3b09850b052edbcb395c6002fb9bb49d7c1356b
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2022-09-06 22:57:02 +0900

    [update] LRU キャッシュを追加

--- src/repository.ts
+++ src/repository.ts
@@ -1,14 +1,19 @@
 import CyclicDB from 'cyclic-dynamodb'
-import { CyclicCollectionItem, PropsBase, Record } from './domain'
+import LRU from 'lru-cache'
+import { CyclicCollectionItem, Record } from './domain'
 import { generateUuidV4 } from './utils'
 
 const db = CyclicDB(process.env.CYCLIC_DB)
 
-const generateId = (): string => {
-  return generateUuidV4().slice(0, 7)
-}
+// 500 record までメモリにキャッシュする
+const cache = new LRU<string, Record | null>({
+  maxSize: 500,
+  sizeCalculation: (_v, _k) => 1,
+})
 
-export const select = async (id: string): Promise<Record> => {
+export const _selectFromDynamoDB = async (
+  id: string
+): Promise<Record | null> => {
   const idToOrigin = db.collection('idToOrigin')
   const cyclicItem: CyclicCollectionItem = await idToOrigin.get(id)
   if (cyclicItem !== null) {
@@ -18,8 +23,32 @@ export const select = async (id: string): Promise<Record> => {
   }
 }
 
+const generateId = async (): Promise<string> => {
+  // 重複の無い id を生成する
+  while (true) {
+    const id = generateUuidV4().slice(0, 7)
+    // cache.get の結果が undefined or null かつ dynamodb にも無い
+    if (!cache.get(id) && (await _selectFromDynamoDB(id)) === null) {
+      return id
+    }
+    console.info('[ID Conflict]', id)
+  }
+}
+
+export const select = async (id: string): Promise<Record | null> => {
+  // キャッシュあり
+  let record = cache.get(id)
+  if (record === undefined) {
+    record = await _selectFromDynamoDB(id)
+    cache.set(id, record) // record が null だったとしても put
+  } else {
+    console.log('[Cache Hit]', id)
+  }
+  return record
+}
+
 export const insert = async (origin: string): Promise<string> => {
-  const id = generateId()
+  const id = await generateId()
   const idToOrigin = db.collection('idToOrigin')
   await idToOrigin.set(id, {
     id,
